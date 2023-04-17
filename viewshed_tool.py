ef2ad237f113891654a72983922c6ed52f8c9764
Jamilton-73516 <114402115+Jamilton-73516@users.noreply.github.com>
2023-04-17 16:46:56 +0100

    added gdal import

--- viewshed_tool.py
+++ viewshed_tool.py
@@ -1,10 +1,11 @@
 #tool to create viewsheds from a DEM raster
 
-
+# library imports
 import numpy as np
 import rasterio as rio
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import rasterstats
+from osgeo import gdal
 
