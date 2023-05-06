aaee774771b7541f9f7fedabd922209d078419e0
Jamilton-73516 <114402115+Jamilton-73516@users.noreply.github.com>
2023-05-06 16:32:01 +0100

    Added: map? Creates plot of LCM Needs: its own function, scalebar fixed, appropriate legend colours rather than randomly assigned, need separate map for buildings

--- viewshed_tool.py
+++ viewshed_tool.py
@@ -2,19 +2,20 @@
 
 # library imports
 import numpy as np
-import rasterio as rio
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 from cartopy.feature import ShapelyFeature
-import rasterstats
 import cartopy.crs as ccrs
 from osgeo import gdal
 from osgeo import osr
 from osgeo import ogr
 from shapely.geometry import Point, LineString, Polygon
 import sys
+import os
 import fiona #to support different drivers for geodataframe to file
 
 #set pycharm display to show all columns of data
@@ -49,17 +50,6 @@ def viewshedcreate():
         options=["UNUSED=YES"],
     )
 viewshedcreate()
-
-#def pixelcount():
-    #vs_pix = gdal.Open('data/outputs/viewshed_northcoast.tif')
-    #band = vs_pix.GetRasterBand(1)
-    #Cols = vs_pix.RasterXSize
-    #Rows = vs_pix.RasterYSize
-    #data = band.ReadAsArray(0, 0, Cols, Rows).astype(float)
-
-    #class1 = np.where(data==255)
-    #print(np.sum(class1))
-#pixelcount()
 
 def rast2poly():
     src_ds_rast = gdal.Open('data/outputs/viewshed_northcoast.tif')
@@ -107,46 +97,157 @@ def lcm_stats():
 
 lcm_stats()
 
-def building_select():
-    bld = gpd.read_file('data/inputs/buildings_binevenagh.shp')
+def point_select():
+    pnt = gpd.read_file('data/inputs/buildings_binevenagh.shp')
     vs = gpd.read_file('data/outputs/viewshed_visible.shp')
-    bld = bld.to_crs(epsg=29902)
+    pnt = pnt.to_crs(epsg=29902)
     vs = vs.to_crs(epsg=29902)
-    bld_visible = gpd.sjoin(bld, vs, how='inner', predicate='within')
-    bld_visible.to_file('data/outputs/buildings_visible.shp')
-building_select()
+    pnt_visible = gpd.sjoin(pnt, vs, how='inner', predicate='within')
+    pnt_visible.to_file('data/outputs/buildings_visible.shp')
+point_select()
 
-def building_stats():
-    bld = gpd.read_file('data/outputs/buildings_visible.shp')
-    bld = bld.to_crs(epsg=29902)
-    print(bld.groupby(['USE'])['USE'].count())
-building_stats()
+def point_stats():
+    pnt = gpd.read_file('data/outputs/buildings_visible.shp')
+    pnt = pnt.to_crs(epsg=29902)
+    print(pnt.groupby(['USE'])['USE'].count())
+point_stats()
 
 #map plotting
-# def makemap():
-#     def gen_handles(labels, colors, edge="k", alpha=1):
-#         lc = lens(colors)
-#         handles = []
-#         for i in range(len(labels)):
-#             handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
-#         return handles
+# def makeanicemap():
+#     outline = gpd.read_file('data/inputs/NI_outline.shp')
+#     outline = outline.to_crs(epsg=4326)
+#     lcm = gpd.read_file('data/outputs/landcover_clipped.shp')
+#     lcm = lcm.to_crs(epsg=4326)
+#     pnt = gpd.read_file('data/outputs/buildings_visible.shp')
+#     pnt = pnt.to_crs(epsg=4326)
 #
-#     def displaymap():
-#         #load files
-#         lcm = gpd.read_file('data/outputs/landcover_clipped.shp')
-#         lcm = lcm.to_crs(epsg=4326)
-#         outline = gpd.read_file('data/outline/NI_outline.shp')
-#         outline = outline.to_crs(epsg=4326)
-#         #create plot
-#         myCRS = ccrs.UTM(29)
-#         fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=(projection=myCRS))
+#     myFig = plt.figure(figsize=(10, 10))
+#     myCRS = ccrs.UTM(29)
+#     ax = plt.axes(projection=myCRS)
 #
-#         outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
-#         xmin, ymin, xmax, ymax = outline.total_bounds
-#         ax.add_feature(outline_feature)
-#
-#         ax.set_extent([xmin-5000, xmax+5000, ymin-5000, ymax+5000], crs=myCRS)
-#         lcm_colors= ["silver", "firebrick", "sandybrown", "olivedrab", "lightseagreen", "navy", "mediumvioletred"]
+#     outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
+#     xmin, ymin, xmax, ymax = outline.total_bounds
+#     ax.add_feature(outline_feature)
+#     ax.set_extent([xmin-5000, xmax+5000, ymin-5000, ymax+5000], crs=myCRS)
+#     #myFig.savefig('maptest2.png', bbox_inches='tight', dpi=300)
+# makeanicemap()
+
+def generate_handles(labels, colors, edge='k', alpha=1):
+    lc = len(colors)  # get the length of the color list
+    handles = []
+    for i in range(len(labels)):
+        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
+    return handles
 
 
+# create a scale bar of length 20 km in the upper right corner of the map
+# adapted this question: https://stackoverflow.com/q/32333870
+# answered by SO user Siyh: https://stackoverflow.com/a/35705477
+def scale_bar(ax, location=(0.5, 0.2)):
+    x0, x1, y0, y1 = ax.get_extent()
+    sbx = x0 + (x1 - x0) * location[0]
+    sby = y0 + (y1 - y0) * location[1]
 
+    ax.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=9, transform=ax.projection)
+    ax.plot([sbx, sbx - 5000], [sby, sby], color='k', linewidth=6, transform=ax.projection)
+    ax.plot([sbx-50000, sbx - 10000], [sby, sby], color='w', linewidth=6, transform=ax.projection)
+
+    ax.text(sbx, sby-4500, '10 km', transform=ax.projection, fontsize=8)
+    ax.text(sbx-12500, sby-4500, '5 km', transform=ax.projection, fontsize=8)
+    ax.text(sbx-24500, sby-4500, '0 km', transform=ax.projection, fontsize=8)
+
+
+# load the outline of Northern Ireland for a backdrop
+outline = gpd.read_file(os.path.abspath('data/inputs/NI_outline.shp'))
+
+# load the datasets
+lcm = gpd.read_file('data/outputs/landcover_clipped.shp')
+pnt = gpd.read_file('data/outputs/buildings_visible.shp')
+
+# create a figure of size 10x10 (representing the page size in inches)
+myFig = plt.figure(figsize=(10, 10))
+
+myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
+# NI is in UTM Zone 29, so we pass 29 to ccrs.UTM()
+
+ax = plt.axes(projection=myCRS)  # finally, create an axes object in the figure, using a UTM projection,
+# where we can actually plot our data.
+
+# first, we just add the outline of Northern Ireland using cartopy's ShapelyFeature
+outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w', alpha=0.7, linewidth=0.7)
+
+xmin, ymin, xmax, ymax = lcm.total_bounds
+ax.add_feature(outline_feature)  # add the features we've created to the map.
+
+# using the boundary of the shapefile features, zoom the map to our area of interest
+ax.set_extent([xmin-1000, xmax+1000, ymin-1000, ymax+1000], crs=myCRS)  # because total_bounds
+# gives output as xmin, ymin, xmax, ymax,
+# but set_extent takes xmin, xmax, ymin, ymax, we re-order the coordinates here.
+
+# pick colors, add features to the map
+lcm_colors = ['firebrick', 'seagreen', 'royalblue', 'coral', 'violet', 'cornsilk', 'pink', 'lime']
+
+# get a list of unique names for the county boundaries
+lcm_cats = list(lcm.category.unique())
+lcm_cats.sort()  # sort the counties alphabetically by name
+
+# next, add the municipal outlines to the map using the colors that we've picked.
+# here, we're iterating over the unique values in the 'CountyName' field.
+# we're also setting the edge color to be black, with a line width of 0.5 pt.
+# Feel free to experiment with different colors and line widths.
+for ii, name in enumerate(lcm_cats):
+    feat = ShapelyFeature(lcm.loc[lcm['category'] == name, 'geometry'],  # first argument is the geometry
+                          myCRS,  # second argument is the CRS
+                          edgecolor='k',  # outline the feature in black
+                          facecolor=lcm_colors[ii],  # set the face color to the corresponding color from the list
+                          linewidth=1,  # set the outline width to be 1 pt
+                          alpha=0.25)  # set the alpha (transparency) to be 0.25 (out of 1)
+    ax.add_feature(feat)  # once we have created the feature, we have to add it to the map using ax.add_feature()
+
+# here, we're setting the edge color to be the same as the face color. Feel free to change this around,
+# and experiment with different line widths.
+# water_feat = ShapelyFeature(water['geometry'],  # first argument is the geometry
+#                             myCRS,  # second argument is the CRS
+#                             edgecolor='mediumblue',  # set the edgecolor to be mediumblue
+#                             facecolor='mediumblue',  # set the facecolor to be mediumblue
+#                             linewidth=1)  # set the outline width to be 1 pt
+# ax.add_feature(water_feat)  # add the collection of features to the map
+
+# river_feat = ShapelyFeature(rivers['geometry'],  # first argument is the geometry
+#                             myCRS,  # second argument is the CRS
+#                             edgecolor='royalblue',  # set the edgecolor to be royalblue
+#                             linewidth=0.2)  # set the linewidth to be 0.2 pt
+# ax.add_feature(river_feat)  # add the collection of features to the map
+
+# # ShapelyFeature creates a polygon, so for point data we can just use ax.plot()
+# town_handle = ax.plot(towns.geometry.x, towns.geometry.y, 's', color='0.5', ms=6, transform=myCRS)
+
+# generate a list of handles for the county datasets
+lcm_handles = generate_handles(lcm.category.unique(), lcm_colors, alpha=0.25)
+
+# # note: if you change the color you use to display lakes, you'll want to change it here, too
+# water_handle = generate_handles(['Lakes'], ['mediumblue'])
+#
+# # note: if you change the color you use to display rivers, you'll want to change it here, too
+# river_handle = [mlines.Line2D([], [], color='royalblue')]  # have to make this a list
+
+# update county_names to take it out of uppercase text
+nice_names = [name.title() for name in lcm_cats]
+
+# ax.legend() takes a list of handles and a list of labels corresponding to the objects you want to add to the legend
+handles = lcm_handles #+ water_handle + river_handle + town_handle
+labels = nice_names #+ ['Lakes', 'Rivers', 'Towns']
+
+leg = ax.legend(handles, labels, title='Legend', title_fontsize=12,
+                fontsize=10, loc='upper left', frameon=True, framealpha=1)
+
+# add the text labels for the towns
+# for ind, row in towns.iterrows():  # towns.iterrows() returns the index and row
+#     x, y = row.geometry.x, row.geometry.y  # get the x,y location for each town
+#     ax.text(x, y, row['TOWN_NAME'].title(), fontsize=8, transform=myCRS)  # use plt.text to place a label at x,y
+
+# add the scale bar to the axis
+scale_bar(ax)
+
+# save the figure as map.png, cropped to the axis (bbox_inches='tight'), and a dpi of 300
+myFig.savefig('data/outputs/maptest5.png', bbox_inches='tight', dpi=300)
